0ebe6e2655cfbcd500599a5904e72ceaefe34482
Dave-gravy <130635257+Raja-666@users.noreply.github.com>
2023-12-21 07:59:17 +0530

    Add files via upload

---
+++ Endpoint.js
@@ -0,0 +1,82 @@
+import {api} from "../store/baseurl";
+ 
+const NeuroNFT = api.injectEndpoints({
+    endpoints: (builder) => ({
+
+
+      nftCreate: builder.mutation({
+        query: (collData) => ({
+          url: "/nft-collection/Createcollection",
+          method: "POST",
+          body: {...collData},
+          formData:true
+        }),
+        invalidatesTags: ["NFTCollection"],
+      }),
+
+
+      SubmitKYC: builder.mutation({
+        query: (NeuroNFT) => ({
+          url: "/kyc/submitKYC",
+          method: "POST",
+          body: NeuroNFT,
+        }),
+        invalidatesTags: ["NFTCollection"],
+      }),
+
+      collectionList: builder.mutation({
+        query: (collData) => ({
+          url: "/nft-collection/CollectionList",
+          method: "POST",
+          body: collData,
+          formData:true
+        }),
+        invalidatesTags: ["NFTCollectionList"],
+      }),
+      
+      createNFT: builder.mutation({
+        query: (collData) => ({
+          url: "/choosecollection/CreateNft",
+          method: "POST",
+          body: collData,
+          formData:true
+        }),
+        invalidatesTags: ["NFTCollectionList"],
+      }),
+
+      homeColList: builder.mutation({
+        query: (collData) => ({
+          url: "/nft-collection/get-collection",
+          method: "POST",
+          body: collData,
+          formData:true
+        }),
+        invalidatesTags: ["home-col-list"],
+      }),
+
+      fullColList: builder.mutation({
+        query: (collData) => ({
+          url: "/nft-collection/view-collection",
+          method: "POST",
+          body: {...collData},
+          formData:true
+        }),
+        invalidatesTags: ["full-col-list"],
+      }),
+
+
+      getNftDetail: builder.mutation({
+        query: (collData) => ({
+          url: "/nft-collection/nft-detail",
+          method: "POST",
+          body: {...collData},
+        }),
+        invalidatesTags: ["single-nft-detail"],
+      }),
+
+
+
+    })
+  });
+  // 
+export const {useNftCreateMutation, useSubmitKYCMutation , useCollectionListMutation, useCreateNFTMutation , useHomeColListMutation , useFullColListMutation , useGetNftDetailMutation} = NeuroNFT;
